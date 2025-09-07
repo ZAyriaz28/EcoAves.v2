7b5a57ed4db227912aad95409bcb183db5273f71
ZAyriaz28 <zayriazrielwilsonsanchez@gmail.com>
2025-09-06 22:11:21 -0600

    Mejora al login

--- js/inicio.js
+++ js/inicio.js
@@ -2,3 +2,9 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   e.preventDefault();
   window.location.href = 'inicio.html';
 });
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Usuario creado'); // Notificación para PC y móviles
+  window.location.href = 'inicio.html';
+});
